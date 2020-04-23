5ac4872f4964dce8867a335c8f65f6b0e3860c8f
Steven <stevenrafaelie@gmail.com>
2020-04-23 11:51:28 +0700

    safe 1

---
+++ Testingnc2/VViewController.swift
@@ -0,0 +1,30 @@
+//
+//  VViewController.swift
+//  Testingnc2
+//
+//  Created by Steven Rafaelie on 12/03/20.
+//  Copyright © 2020 Apple academy. All rights reserved.
+//
+
+import UIKit
+
+class VViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Testingnc2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Testingnc2
+//
+//  Created by Steven Rafaelie on 12/03/20.
+//  Copyright © 2020 Apple academy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
